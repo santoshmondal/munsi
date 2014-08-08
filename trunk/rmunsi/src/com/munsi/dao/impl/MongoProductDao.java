205d61b8ff6c1421c18dcf8986a8923b608e2b96
santosh.ece06@gmail.com <santosh.ece06@gmail.com@7f288099-d04c-66d2-1357-36c1d3b5ebf3>
2014-08-08 19:40:42 +0000

    list query has been updated.

--- trunk/rmunsi/src/com/munsi/dao/impl/MongoProductDao.java
+++ trunk/rmunsi/src/com/munsi/dao/impl/MongoProductDao.java
@@ -22,121 +22,119 @@ import com.munsi.util.Constants.DBCollectionEnum;
 import com.munsi.util.MongoUtil;
 
 public class MongoProductDao implements ProductDao {
-	private static final Logger LOG = Logger.getLogger( MongoProductDao.class );
-	
+	private static final Logger LOG = Logger.getLogger(MongoProductDao.class);
+
 	public static final String KEY_MANUFACTURER_XID = "manufacturerXid";
 	public static final String KEY_TAX_LIST_XID = "taxListXid";
 	public static final String KEY_PRODUCT_GROUP_XID = "productGroupXid";
 	public static final String KEY_PRODUCT_SUBGROUP_XID = "productSubGroupXid";
-	
+
 	public static final String KEY_MANUFACTURER = "manufacturer";
 	public static final String KEY_PRODUCT_GROUP = "productGroup";
 	public static final String KEY_PRODUCT_SUBGROUP = "productSubGroup";
-	public static final String KEY_TAX_LIST= "taxList";
-	
-	
+	public static final String KEY_TAX_LIST = "taxList";
+
 	private String collProduct = DBCollectionEnum.MAST_PRODUCT.toString();
 	private String collManufacturer = DBCollectionEnum.MAST_MANUFACTURER.toString();
 	private String collTax = DBCollectionEnum.MAST_TAX.toString();
 	private String collProductGroup = DBCollectionEnum.MAST_PRODUCT_GROUP.toString();
-	
+
 	private DB mongoDB = MongoUtil.getDB();
-	
+
 	@Override
 	public Boolean create(Product product) {
-		try{
+		try {
 			Date date = new Date();
-			product.setCtime( date );
-			product.setUtime( date );
+			product.setCtime(date);
+			product.setUtime(date);
 			String _id = MongoUtil.getNextSequence(DBCollectionEnum.MAST_PRODUCT).toString();
-			product.set_id( _id );
-			
-			DBCollection collection = mongoDB.getCollection( collProduct );
+			product.set_id(_id);
+
+			DBCollection collection = mongoDB.getCollection(collProduct);
 			String jsonString = CommonUtil.objectToJson(product);
-			
-			DBObject dbObject = (DBObject) JSON.parse( jsonString );
-			
+
+			DBObject dbObject = (DBObject) JSON.parse(jsonString);
+
 			BasicDBList basicDBList = new BasicDBList();
-			if( product.getTaxList() != null ){
-				for(Tax tax : product.getTaxList() ){
-					DBRef taxRef = new DBRef(mongoDB, collTax, tax.get_id() );
+			if (product.getTaxList() != null) {
+				for (Tax tax : product.getTaxList()) {
+					DBRef taxRef = new DBRef(mongoDB, collTax, tax.get_id());
 					basicDBList.add(taxRef);
 				}
 			}
-			
-			
-			DBRef manufacturerRef = new DBRef(mongoDB, collManufacturer, product.getManufacturer().get_id() );
+
+			DBRef manufacturerRef = new DBRef(mongoDB, collManufacturer, product.getManufacturer().get_id());
 			DBRef productGroupRef = new DBRef(mongoDB, collProductGroup, product.getProductGroup().get_id());
 			DBRef productSubGroupRef = new DBRef(mongoDB, collProductGroup, product.getProductSubGroup().get_id());
-			
-			dbObject.put( KEY_TAX_LIST_XID, basicDBList);
-			dbObject.put( KEY_MANUFACTURER_XID, manufacturerRef );
-			dbObject.put( KEY_PRODUCT_GROUP_XID, productGroupRef );
-			dbObject.put( KEY_PRODUCT_SUBGROUP_XID, productSubGroupRef );
-			
+
+			dbObject.put(KEY_TAX_LIST_XID, basicDBList);
+			dbObject.put(KEY_MANUFACTURER_XID, manufacturerRef);
+			dbObject.put(KEY_PRODUCT_GROUP_XID, productGroupRef);
+			dbObject.put(KEY_PRODUCT_SUBGROUP_XID, productSubGroupRef);
+
 			dbObject.removeField(KEY_MANUFACTURER);
 			dbObject.removeField(KEY_TAX_LIST);
 			dbObject.removeField(KEY_PRODUCT_GROUP);
 			dbObject.removeField(KEY_PRODUCT_SUBGROUP);
-			
-			collection.insert(dbObject );
+
+			collection.insert(dbObject);
 			return true;
-			
-		}catch( Exception exception ){
+
+		} catch (Exception exception) {
 			LOG.equals(exception);
 		}
 		return false;
 	}
-	
+
 	@Override
 	public Boolean update(Product product) {
-		try{
+		try {
 			Date date = new Date();
-			product.setUtime( date );
-			
-			DBCollection collection = mongoDB.getCollection( collProduct );
+			product.setUtime(date);
+
+			DBCollection collection = mongoDB.getCollection(collProduct);
 			String jsonString = CommonUtil.objectToJson(product);
-			
-			DBObject dbObject = (DBObject) JSON.parse( jsonString );
-			if( product.getTaxList() != null ){
+
+			DBObject dbObject = (DBObject) JSON.parse(jsonString);
+			if (product.getTaxList() != null) {
 				BasicDBList basicDBList = new BasicDBList();
-				for(Tax tax : product.getTaxList() ){
-					DBRef taxRef = new DBRef(mongoDB, collTax, tax.get_id() );
+				for (Tax tax : product.getTaxList()) {
+					DBRef taxRef = new DBRef(mongoDB, collTax, tax.get_id());
 					basicDBList.add(taxRef);
 				}
-				dbObject.put( KEY_TAX_LIST_XID, basicDBList);
+				dbObject.put(KEY_TAX_LIST_XID, basicDBList);
 				dbObject.removeField(KEY_TAX_LIST);
 			}
-			if( product.getManufacturer() != null ){
-				DBRef manufacturerRef = new DBRef(mongoDB, collManufacturer, product.getManufacturer().get_id() );
-				dbObject.put( KEY_MANUFACTURER_XID, manufacturerRef );
+			if (product.getManufacturer() != null) {
+				DBRef manufacturerRef = new DBRef(mongoDB, collManufacturer, product.getManufacturer().get_id());
+				dbObject.put(KEY_MANUFACTURER_XID, manufacturerRef);
 				dbObject.removeField(KEY_MANUFACTURER);
 			}
-			if(product.getProductGroup() != null){
+			if (product.getProductGroup() != null) {
 				DBRef productGroupRef = new DBRef(mongoDB, collProductGroup, product.getProductGroup().get_id());
-				dbObject.put( KEY_PRODUCT_GROUP_XID, productGroupRef );
+				dbObject.put(KEY_PRODUCT_GROUP_XID, productGroupRef);
 				dbObject.removeField(KEY_PRODUCT_GROUP);
 			}
-			if( product.getProductSubGroup() != null ){
+			if (product.getProductSubGroup() != null) {
 				DBRef productSubGroupRef = new DBRef(mongoDB, collProductGroup, product.getProductSubGroup().get_id());
-				dbObject.put( KEY_PRODUCT_SUBGROUP_XID, productSubGroupRef );
+				dbObject.put(KEY_PRODUCT_SUBGROUP_XID, productSubGroupRef);
 				dbObject.removeField(KEY_PRODUCT_SUBGROUP);
 			}
 			dbObject.removeField("_id");
-			
+
 			DBObject query = new BasicDBObject("_id", product.get_id());
-			DBObject updateObj = new BasicDBObject("$set", dbObject); 
-			
+			DBObject updateObj = new BasicDBObject("$set", dbObject);
+
 			collection.update(query, updateObj);
 			return true;
-			
-		}catch( Exception exception ){
+
+		} catch (Exception exception) {
 			LOG.equals(exception);
 		}
 		return false;
-		
+
 	}
-	
+
 	@Override
 	public Boolean delete(String _id) {
 		Product product = new Product();
@@ -145,179 +143,177 @@ public class MongoProductDao implements ProductDao {
 		product.setUtime(new Date());
 		return update(product);
 	}
-	
+
 	@Override
 	public Product get(String _id) {
-		return get(_id, false); // _id of product, withReferences - false 
+		return get(_id, false); // _id of product, withReferences - false
 	}
-	
+
 	@Override
 	public Product get(String _id, Boolean withReferences) {
-		try{
-			DBCollection collection = mongoDB.getCollection( collProduct );
+		try {
+			DBCollection collection = mongoDB.getCollection(collProduct);
 			DBObject query = new BasicDBObject("_id", _id);
 			DBObject dbObject = collection.findOne(query);
-			
-			if( withReferences == true ){
+
+			if (withReferences == true) {
 				BasicDBList basicDBList = (BasicDBList) dbObject.get(KEY_TAX_LIST_XID);
-				
-				BasicDBList taxDBOList =  new BasicDBList();
-				if( basicDBList != null ){
-					DBRef [] taxDBRefList = (DBRef[]) basicDBList.toArray();
-					
-					for(DBRef taxDBRef : taxDBRefList ){
+
+				BasicDBList taxDBOList = new BasicDBList();
+				if (basicDBList != null) {
+					DBRef[] taxDBRefList = (DBRef[]) basicDBList.toArray();
+
+					for (DBRef taxDBRef : taxDBRefList) {
 						DBObject taxDBObject = taxDBRef.fetch();
 						taxDBOList.add(taxDBObject);
 					}
 				}
-				
-				DBObject manufacturerDBO =  ( (DBRef)dbObject.get(KEY_MANUFACTURER_XID) ).fetch();
-				DBObject productGroupDBO =  ( (DBRef)dbObject.get(KEY_PRODUCT_GROUP_XID) ).fetch();
-				DBObject productSubGroupDBO =  ( (DBRef)dbObject.get(KEY_PRODUCT_SUBGROUP_XID) ).fetch();
-				
+
+				DBObject manufacturerDBO = ((DBRef) dbObject.get(KEY_MANUFACTURER_XID)).fetch();
+				DBObject productGroupDBO = ((DBRef) dbObject.get(KEY_PRODUCT_GROUP_XID)).fetch();
+				DBObject productSubGroupDBO = ((DBRef) dbObject.get(KEY_PRODUCT_SUBGROUP_XID)).fetch();
+
 				dbObject.put(KEY_TAX_LIST, taxDBOList);
 				dbObject.put(KEY_MANUFACTURER, manufacturerDBO);
 				dbObject.put(KEY_PRODUCT_GROUP, productGroupDBO);
 				dbObject.put(KEY_PRODUCT_SUBGROUP, productSubGroupDBO);
 			}
-			
+
 			dbObject.removeField(KEY_TAX_LIST_XID);
 			dbObject.removeField(KEY_MANUFACTURER_XID);
 			dbObject.removeField(KEY_PRODUCT_GROUP_XID);
 			dbObject.removeField(KEY_PRODUCT_SUBGROUP_XID);
-			
+
 			String jsonString = JSON.serialize(dbObject);
-			Product product = (Product) CommonUtil.jsonToObject( jsonString, Product.class.getName() );
-			
+			Product product = (Product) CommonUtil.jsonToObject(jsonString, Product.class.getName());
+
 			return product;
-			
-		}catch( Exception exception ){
-			LOG.equals(exception);
-		}
-		return null;
-	}
-	
-	@Override
-	public List<Product> getAll() {
-		return getAll(false);
-	}
-	
-	@Override
-	public List<Product> getAll(Boolean withReferences) {
-		try{
-			DBCollection collection = mongoDB.getCollection( collProduct );
-			DBObject deletedQuery = MongoUtil.getQueryToCheckDeleted();
-			DBCursor dbCursor = collection.find(deletedQuery);
-			
-			List<Product> productList = new ArrayList<>();
-			
-			while ( dbCursor.hasNext() ) {
-				DBObject dbObject = dbCursor.next();
-				
-				if( withReferences == true ){
-					
-					BasicDBList basicDBList = (BasicDBList) dbObject.get(KEY_TAX_LIST_XID);
-					
-					BasicDBList taxDBOList =  new BasicDBList();
-					if( basicDBList != null ){
-						DBRef [] taxDBRefList = (DBRef[]) basicDBList.toArray();
-						
-						for(DBRef taxDBRef : taxDBRefList ){
-							DBObject taxDBObject = taxDBRef.fetch();
-							taxDBOList.add(taxDBObject);
-						}
-					}
-					
-					DBObject manufacturerDBO =  ( (DBRef)dbObject.get(KEY_MANUFACTURER_XID) ).fetch();
-					DBObject productGroupDBO =  ( (DBRef)dbObject.get(KEY_PRODUCT_GROUP_XID) ).fetch();
-					DBObject productSubGroupDBO =  ( (DBRef)dbObject.get(KEY_PRODUCT_SUBGROUP_XID) ).fetch();
-					
-					dbObject.put(KEY_TAX_LIST, taxDBOList);
-					dbObject.put(KEY_MANUFACTURER_XID, manufacturerDBO);
-					dbObject.put(KEY_PRODUCT_GROUP, productGroupDBO);
-					dbObject.put(KEY_PRODUCT_SUBGROUP, productSubGroupDBO);
-					
-				}
-				
-				dbObject.removeField(KEY_TAX_LIST_XID);
-				dbObject.removeField(KEY_MANUFACTURER_XID);
-				dbObject.removeField(KEY_PRODUCT_GROUP_XID);
-				dbObject.removeField(KEY_PRODUCT_SUBGROUP_XID);
-				
-				String jsonString = JSON.serialize(dbObject);
-				Product product = (Product) CommonUtil.jsonToObject( jsonString, Product.class.getName() );
-				productList.add(product);
-			}
-			
-			return productList;
-			
-		}catch( Exception exception ){
+
+		} catch (Exception exception) {
 			LOG.equals(exception);
 		}
 		return null;
 	}
 
 	@Override
-	public Product getProductByCode(String code, Boolean withReferences){
+	public List<Product> getAll() {
+		return getAll(false);
+	}
+
+	@Override
+	public List<Product> getAll(Boolean withReferences) {
+		try {
+			DBCollection collection = mongoDB.getCollection(collProduct);
+			DBObject deletedQuery = MongoUtil.getQueryToCheckDeleted();
+			DBCursor dbCursor = collection.find(deletedQuery);
+
+			List<Product> productList = new ArrayList<>();
+
+			while (dbCursor.hasNext()) {
+				DBObject dbObject = dbCursor.next();
+
+				if (withReferences == true) {
+
+					BasicDBList basicDBList = (BasicDBList) dbObject.get(KEY_TAX_LIST_XID);
+
+					BasicDBList taxDBOList = new BasicDBList();
+					if (basicDBList != null) {
+						DBRef[] taxDBRefList = (DBRef[]) basicDBList.toArray();
+
+						for (DBRef taxDBRef : taxDBRefList) {
+							DBObject taxDBObject = taxDBRef.fetch();
+							taxDBOList.add(taxDBObject);
+						}
+					}
+
+					DBObject manufacturerDBO = ((DBRef) dbObject.get(KEY_MANUFACTURER_XID)).fetch();
+					DBObject productGroupDBO = ((DBRef) dbObject.get(KEY_PRODUCT_GROUP_XID)).fetch();
+					DBObject productSubGroupDBO = ((DBRef) dbObject.get(KEY_PRODUCT_SUBGROUP_XID)).fetch();
+
+					dbObject.put(KEY_TAX_LIST, taxDBOList);
+					dbObject.put(KEY_MANUFACTURER_XID, manufacturerDBO);
+					dbObject.put(KEY_PRODUCT_GROUP, productGroupDBO);
+					dbObject.put(KEY_PRODUCT_SUBGROUP, productSubGroupDBO);
+
+				}
+
+				dbObject.removeField(KEY_TAX_LIST_XID);
+				dbObject.removeField(KEY_MANUFACTURER_XID);
+				dbObject.removeField(KEY_PRODUCT_GROUP_XID);
+				dbObject.removeField(KEY_PRODUCT_SUBGROUP_XID);
+
+				String jsonString = JSON.serialize(dbObject);
+				Product product = (Product) CommonUtil.jsonToObject(jsonString, Product.class.getName());
+				productList.add(product);
+			}
+
+			return productList;
+
+		} catch (Exception exception) {
+			LOG.equals(exception);
+		}
+		return null;
+	}
+
+	@Override
+	public Product getProductByCode(String code, Boolean withReferences) {
 		DBObject queryObject = new BasicDBObject("code", code);
 		return getProductByQuery(queryObject, withReferences);
 	}
 
 	@Override
-	public Product getProductByBarCode(String barCode, Boolean withReferences){
+	public Product getProductByBarCode(String barCode, Boolean withReferences) {
 		DBObject queryObject = new BasicDBObject("barCode", barCode);
 		return getProductByQuery(queryObject, withReferences);
 	}
 
 	@Override
-	public Product getProductByName(String name, Boolean withReferences){
+	public Product getProductByName(String name, Boolean withReferences) {
 		DBObject queryObject = new BasicDBObject("name", name);
 		return getProductByQuery(queryObject, withReferences);
 	}
-	
 
 	private Product getProductByQuery(DBObject queryObject, Boolean withReferences) {
-		try{
-			DBCollection collection = mongoDB.getCollection( collProduct );
+		try {
+			DBCollection collection = mongoDB.getCollection(collProduct);
 			DBObject dbObject = collection.findOne(queryObject);
-			
-			if( withReferences == true ){
-				BasicDBList basicDBList = (BasicDBList) dbObject.get(KEY_TAX_LIST_XID);
-				
-				BasicDBList taxDBOList =  new BasicDBList();
-				if( basicDBList != null ){
-					DBRef [] taxDBRefList = (DBRef[]) basicDBList.toArray();
-					
-					for(DBRef taxDBRef : taxDBRefList ){
+
+			if (withReferences == true) {
+				@SuppressWarnings("unchecked")
+				List<DBRef> basicDBList = (List<DBRef>) dbObject.get(KEY_TAX_LIST_XID);
+
+				BasicDBList taxDBOList = new BasicDBList();
+				if (basicDBList != null) {
+					for (DBRef taxDBRef : basicDBList) {
 						DBObject taxDBObject = taxDBRef.fetch();
 						taxDBOList.add(taxDBObject);
 					}
 				}
-				
-				DBObject manufacturerDBO =  ( (DBRef)dbObject.get(KEY_MANUFACTURER_XID) ).fetch();
-				DBObject productGroupDBO =  ( (DBRef)dbObject.get(KEY_PRODUCT_GROUP_XID) ).fetch();
-				DBObject productSubGroupDBO =  ( (DBRef)dbObject.get(KEY_PRODUCT_SUBGROUP_XID) ).fetch();
-				
+
+				DBObject manufacturerDBO = ((DBRef) dbObject.get(KEY_MANUFACTURER_XID)).fetch();
+				DBObject productGroupDBO = ((DBRef) dbObject.get(KEY_PRODUCT_GROUP_XID)).fetch();
+				DBObject productSubGroupDBO = ((DBRef) dbObject.get(KEY_PRODUCT_SUBGROUP_XID)).fetch();
+
 				dbObject.put(KEY_TAX_LIST, taxDBOList);
 				dbObject.put(KEY_MANUFACTURER, manufacturerDBO);
 				dbObject.put(KEY_PRODUCT_GROUP, productGroupDBO);
 				dbObject.put(KEY_PRODUCT_SUBGROUP, productSubGroupDBO);
 			}
-			
+
 			dbObject.removeField(KEY_TAX_LIST_XID);
 			dbObject.removeField(KEY_MANUFACTURER_XID);
 			dbObject.removeField(KEY_PRODUCT_GROUP_XID);
 			dbObject.removeField(KEY_PRODUCT_SUBGROUP_XID);
-			
+
 			String jsonString = JSON.serialize(dbObject);
-			Product product = (Product) CommonUtil.jsonToObject( jsonString, Product.class.getName() );
-			
+			Product product = (Product) CommonUtil.jsonToObject(jsonString, Product.class.getName());
+
 			return product;
-			
-		}catch( Exception exception ){
+
+		} catch (Exception exception) {
 			LOG.equals(exception);
 		}
 		return null;
 	}
-	
+
 }

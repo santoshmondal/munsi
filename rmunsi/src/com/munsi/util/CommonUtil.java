70f0cec128b6c3e37b5a088e1ee948f1bfba308e
santosh.ece06@gmail.com <santosh.ece06@gmail.com@7f288099-d04c-66d2-1357-36c1d3b5ebf3>
2014-08-08 19:41:50 +0000

    common util supports taxsum.

--- rmunsi/src/com/munsi/util/CommonUtil.java
+++ rmunsi/src/com/munsi/util/CommonUtil.java
@@ -22,17 +22,18 @@ import com.mongodb.QueryOperators;
 import com.mongodb.util.JSON;
 import com.munsi.dao.ProductDao;
 import com.munsi.pojo.master.Product;
+import com.munsi.pojo.master.Tax;
 import com.munsi.util.Constants.DBCollectionEnum;
 import com.munsi.util.ObjectFactory.ObjectEnum;
 
 public class CommonUtil {
-	private static final Logger LOG = Logger.getLogger( CommonUtil.class );
+	private static final Logger LOG = Logger.getLogger(CommonUtil.class);
 
-	private static Map<String,String> vatTypemap = new LinkedHashMap<>();
-	private static Map<String,String> locationMap = new LinkedHashMap<>();
-	private static Map<String,String> schemeOnMap = new LinkedHashMap<>();
-	private static Map<String,String> schemeTypeMap = new LinkedHashMap<>();
-	
+	private static Map<String, String> vatTypemap = new LinkedHashMap<>();
+	private static Map<String, String> locationMap = new LinkedHashMap<>();
+	private static Map<String, String> schemeOnMap = new LinkedHashMap<>();
+	private static Map<String, String> schemeTypeMap = new LinkedHashMap<>();
+
 	static {
 		// Put value in location map
 		locationMap.put("TE+", "TE+ Trading Expense +ve");
@@ -45,7 +46,7 @@ public class CommonUtil {
 		locationMap.put("BA-", "BA- Balance Sheet Asset -ve");
 		locationMap.put("BL+", "BL+ Balance Sheet Lblty +ve");
 		locationMap.put("BL-", "BL- Balance Sheet Lblty -ve");
-		
+
 		// Put value in vat type map
 		vatTypemap.put("1", Config.getProperty("vatType.1"));
 		vatTypemap.put("2", Config.getProperty("vatType.2"));
@@ -55,169 +56,162 @@ public class CommonUtil {
 		// Put value in scheme type map
 		schemeTypeMap.put("1", Config.getProperty("schemeType.1"));
 		schemeTypeMap.put("2", Config.getProperty("schemeType.2"));
-		
+
 		// Put value in scheme on map
 		schemeOnMap.put("1", Config.getProperty("schemeOn.1"));
 		schemeOnMap.put("2", Config.getProperty("schemeOn.2"));
-		
-	}
-	
-	public static Object jsonToObject(String json, String fullyQualifiedClassName) {
-        ObjectMapper mapper = new ObjectMapper();
 
-        try {
-    		mapper.configure(DeserializationConfig.Feature.FAIL_ON_UNKNOWN_PROPERTIES,false);
-    	    Class<?> jsonClass = Class.forName(fullyQualifiedClassName);
-            return mapper.readValue(json, jsonClass);
-        } catch (JsonGenerationException e) {
-                LOG.error(e);
-        } catch (JsonMappingException e) {
-                LOG.error(e);
-        } catch (IOException e) {
-                LOG.error(e);
-        } catch (ClassNotFoundException e) {
-                LOG.error(e);
-        }
-        return null;
+	}
+
+	public static Object jsonToObject(String json, String fullyQualifiedClassName) {
+		ObjectMapper mapper = new ObjectMapper();
+
+		try {
+			mapper.configure(DeserializationConfig.Feature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+			Class<?> jsonClass = Class.forName(fullyQualifiedClassName);
+			return mapper.readValue(json, jsonClass);
+		} catch (JsonGenerationException e) {
+			LOG.error(e);
+		} catch (JsonMappingException e) {
+			LOG.error(e);
+		} catch (IOException e) {
+			LOG.error(e);
+		} catch (ClassNotFoundException e) {
+			LOG.error(e);
+		}
+		return null;
 	}
 
 	public static String objectToJson(Object object) {
-		
-		if (object instanceof Product)
-		{
-			
+
+		if (object instanceof Product) {
+
 		}
 		ObjectMapper mapper = new ObjectMapper();
-		
-        try {
-        	mapper.setSerializationInclusion(Inclusion.NON_NULL);
-        	mapper.setSerializationInclusion(Inclusion.NON_EMPTY);
-        	
-                return mapper.writeValueAsString(object);
-        } catch (JsonGenerationException e) {
-                LOG.error(e);
-        } catch (JsonMappingException e) {
-                LOG.error(e);
-        } catch (IOException e) {
-                LOG.error(e);
-        }
 
-        return null;
+		try {
+			mapper.setSerializationInclusion(Inclusion.NON_NULL);
+			mapper.setSerializationInclusion(Inclusion.NON_EMPTY);
+
+			return mapper.writeValueAsString(object);
+		} catch (JsonGenerationException e) {
+			LOG.error(e);
+		} catch (JsonMappingException e) {
+			LOG.error(e);
+		} catch (IOException e) {
+			LOG.error(e);
+		}
+
+		return null;
 	}
-	
+
 	public static String generateInvoiceNumber() {
 		return "";
 	}
-	
-	
-	
-	public static String getVatTypeJSON(){
-		
-		
-		StringBuffer sb = new StringBuffer();
-		String separater = "";
-		
-		for( Entry<String, String> entry : vatTypemap.entrySet() ){
-			sb.append(separater);
-			sb.append(entry.getKey());
-			sb.append(":");
-			sb.append(entry.getValue());
-			separater= ";";
-		}
-		
-	    return sb.toString();
-	}
-	
-	
-	
-	public static String getSchemeTypeJSON(){
-		
-		StringBuffer sb = new StringBuffer();
-		String separater = "";
-		
-		for( Entry<String, String> entry : schemeTypeMap.entrySet() ){
-			sb.append(separater);
-			sb.append(entry.getKey());
-			sb.append(":");
-			sb.append(entry.getValue());
-			separater= ";";
-		}
-		
-		return sb.toString();
-	}
-	
-	public static String getSchemeONJSON(){
-		
+
+	public static String getVatTypeJSON() {
 
 		StringBuffer sb = new StringBuffer();
 		String separater = "";
-		
-		for( Entry<String, String> entry : schemeOnMap.entrySet() ){
+
+		for (Entry<String, String> entry : vatTypemap.entrySet()) {
 			sb.append(separater);
 			sb.append(entry.getKey());
 			sb.append(":");
 			sb.append(entry.getValue());
-			separater= ";";
+			separater = ";";
 		}
-		
+
 		return sb.toString();
 	}
-	
-	public static String getLocationString(){
+
+	public static String getSchemeTypeJSON() {
+
 		StringBuffer sb = new StringBuffer();
 		String separater = "";
-		
-		for( Entry<String, String> entry : locationMap.entrySet() ){
+
+		for (Entry<String, String> entry : schemeTypeMap.entrySet()) {
 			sb.append(separater);
 			sb.append(entry.getKey());
 			sb.append(":");
 			sb.append(entry.getValue());
-			separater= ";";
+			separater = ";";
 		}
-	    
+
 		return sb.toString();
 	}
-	
-	public static String getLocationValue(String locCode){
+
+	public static String getSchemeONJSON() {
+
+		StringBuffer sb = new StringBuffer();
+		String separater = "";
+
+		for (Entry<String, String> entry : schemeOnMap.entrySet()) {
+			sb.append(separater);
+			sb.append(entry.getKey());
+			sb.append(":");
+			sb.append(entry.getValue());
+			separater = ";";
+		}
+
+		return sb.toString();
+	}
+
+	public static String getLocationString() {
+		StringBuffer sb = new StringBuffer();
+		String separater = "";
+
+		for (Entry<String, String> entry : locationMap.entrySet()) {
+			sb.append(separater);
+			sb.append(entry.getKey());
+			sb.append(":");
+			sb.append(entry.getValue());
+			separater = ";";
+		}
+
+		return sb.toString();
+	}
+
+	public static String getLocationValue(String locCode) {
 		return locationMap.get(locCode);
 	}
-	
-	
-	public static String getIdNameString(DBCollectionEnum dbCollectionEnum, String valueKey, String lableKey){
+
+	public static String getIdNameString(DBCollectionEnum dbCollectionEnum, String valueKey, String lableKey) {
 		return getIdNameString(dbCollectionEnum, valueKey, lableKey, null);
 	}
-	
+
 	/**
-	 * @param dbCollectionEnum collection name
-	 * @param valueKey 
+	 * @param dbCollectionEnum
+	 *            collection name
+	 * @param valueKey
 	 * @param lableKey
 	 * @param (Optional) jsonQuery : Condition string in json format of Mongo DB
 	 * @return
 	 */
-	public static String getIdNameString(DBCollectionEnum dbCollectionEnum, String valueKey, String lableKey, String jsonQuery){
-		try{
+	public static String getIdNameString(DBCollectionEnum dbCollectionEnum, String valueKey, String lableKey, String jsonQuery) {
+		try {
 			DB mongoDB = MongoUtil.getDB();
-			
-			DBCollection collection = mongoDB.getCollection(dbCollectionEnum.toString() );
-			DBObject dbKey = new BasicDBObject(valueKey,1).append(lableKey, 1);
+
+			DBCollection collection = mongoDB.getCollection(dbCollectionEnum.toString());
+			DBObject dbKey = new BasicDBObject(valueKey, 1).append(lableKey, 1);
 
 			DBObject deletedQuery = MongoUtil.getQueryToCheckDeleted();
-			DBObject finalQuery = deletedQuery; 
-			
-			if( jsonQuery != null && jsonQuery.length() > 0){
+			DBObject finalQuery = deletedQuery;
+
+			if (jsonQuery != null && jsonQuery.length() > 0) {
 				DBObject optionalQuery = (DBObject) JSON.parse(jsonQuery);
 				BasicDBList queryList = new BasicDBList();
 				queryList.add(deletedQuery);
 				queryList.add(optionalQuery);
 				finalQuery = new BasicDBObject(QueryOperators.AND, queryList);
 			}
-			
+
 			DBCursor dbCursor = collection.find(finalQuery, dbKey);
-			
+
 			StringBuffer sb = new StringBuffer();
 			String separater = "";
-			while ( dbCursor.hasNext() ) {
-				
+			while (dbCursor.hasNext()) {
+
 				BasicDBObject dbObject = (BasicDBObject) dbCursor.next();
 				String value = dbObject.getString(valueKey);
 				String name = dbObject.getString(lableKey);
@@ -227,86 +221,103 @@ public class CommonUtil {
 				sb.append(name);
 				separater = ";";
 			}
-				
+
 			return sb.toString();
-				
-			}catch( Exception exception ){
-				exception.printStackTrace();
-			}
+
+		} catch (Exception exception) {
+			exception.printStackTrace();
+		}
 
 		return "";
 	}
-	
 
-	public static String getIdLabelJSON(DBCollectionEnum dbCollectionEnum, String idKey, String lableKey, String jsonQuery){
-		try{
+	public static String getIdLabelJSON(DBCollectionEnum dbCollectionEnum, String idKey, String lableKey, String jsonQuery) {
+		try {
 			DB mongoDB = MongoUtil.getDB();
-			
-			DBCollection collection = mongoDB.getCollection(dbCollectionEnum.toString() );
-			DBObject dbKey = new BasicDBObject(idKey,1).append(lableKey, 1);
+
+			DBCollection collection = mongoDB.getCollection(dbCollectionEnum.toString());
+			DBObject dbKey = new BasicDBObject(idKey, 1).append(lableKey, 1);
 
 			DBObject deletedQuery = MongoUtil.getQueryToCheckDeleted();
-			DBObject finalQuery = deletedQuery; 
-			
+			DBObject finalQuery = deletedQuery;
+
 			BasicDBList queryList = new BasicDBList();
 			queryList.add(new BasicDBObject(idKey, new BasicDBObject("$exists", true)));
 			queryList.add(new BasicDBObject(lableKey, new BasicDBObject("$exists", true)));
-			if( jsonQuery != null && jsonQuery.length() > 0){
+			if (jsonQuery != null && jsonQuery.length() > 0) {
 				DBObject optionalQuery = (DBObject) JSON.parse(jsonQuery);
 				queryList.add(deletedQuery);
 				queryList.add(optionalQuery);
 			}
 			finalQuery = new BasicDBObject(QueryOperators.AND, queryList);
-			
+
 			DBCursor dbCursor = collection.find(finalQuery, dbKey);
 
 			String sb = "[]";
-			if( dbCursor.hasNext() ) {
-				sb=JSON.serialize(dbCursor).toString();
+			if (dbCursor.hasNext()) {
+				sb = JSON.serialize(dbCursor).toString();
 			}
-				
+
 			return sb;
-				
-			}catch( Exception exception ){
-				exception.printStackTrace();
-			}
+
+		} catch (Exception exception) {
+			exception.printStackTrace();
+		}
 
 		return "";
 	}
-	
-	public static String getProductByCode(String code, Boolean withReferences){
-		ProductDao instance = (ProductDao)ObjectFactory.getInstance(ObjectEnum.PRODUCT_DAO);
-		Product product =  instance.getProductByCode(code, withReferences);
-		
-		String productJson = null;
-		if(product != null) {
-			productJson = CommonUtil.objectToJson(product);
-		}
-		
-		return productJson;
+
+	public static String getProductByCode(String code, Boolean withReferences) {
+		ProductDao instance = (ProductDao) ObjectFactory.getInstance(ObjectEnum.PRODUCT_DAO);
+		Product product = instance.getProductByCode(code, withReferences);
+		return populateDerivedProductInfo(product);
+
 	}
-	
-	public static String getProductByBarCode(String barCode, Boolean withReferences){
-		ProductDao instance = (ProductDao)ObjectFactory.getInstance(ObjectEnum.PRODUCT_DAO);
-		Product product =  instance.getProductByBarCode(barCode, withReferences);
-		
-		String productJson = null;
-		if(product != null) {
-			productJson = CommonUtil.objectToJson(product);
-		}
-		
-		return productJson;
+
+	public static String getProductByBarCode(String barCode, Boolean withReferences) {
+		ProductDao instance = (ProductDao) ObjectFactory.getInstance(ObjectEnum.PRODUCT_DAO);
+		Product product = instance.getProductByBarCode(barCode, withReferences);
+		return populateDerivedProductInfo(product);
+
 	}
-	
-	public static String getProductByName(String name, Boolean withReferences){
-		ProductDao instance = (ProductDao)ObjectFactory.getInstance(ObjectEnum.PRODUCT_DAO);
+
+	public static String getProductByName(String name, Boolean withReferences) {
+		ProductDao instance = (ProductDao) ObjectFactory.getInstance(ObjectEnum.PRODUCT_DAO);
 		Product product = instance.getProductByName(name, withReferences);
-		
+		return populateDerivedProductInfo(product);
+
+	}
+
+	private static String populateDerivedProductInfo(Product product) {
 		String productJson = null;
-		if(product != null) {
+		if (product != null) {
+
+			// populate tax
+			Float derSumOfProudctTax = 0.0f;
+			if (product.getTaxList() != null) {
+				for (Tax tax : product.getTaxList()) {
+					if (tax.getRate() != null) {
+						try {
+							Float taxRate = Float.valueOf(tax.getRate());
+							derSumOfProudctTax += taxRate;
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+					}
+				}
+			}
+			// populate tax done
+
+			product.setDerSumOfProudctTax(derSumOfProudctTax);
+
+			// convert to json
 			productJson = CommonUtil.objectToJson(product);
 		}
-		
+
 		return productJson;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(getProductByName("Amul Milk", true));
 	}
 }
